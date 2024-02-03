e34b68138f67fd6d02e70f9663519fce9b52fb19
rakshitha <allurirakshithaa@gmail.com>
2024-02-03 10:07:50 +0000

    visited countries

---
+++ src/components/CountryItem/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable react/no-unknown-property */
+import './index.css'
+
+const CountryItem = props => {
+  const {countryDetails, onRemoveItem} = props
+  const {id, name, imageUrl, isVisited} = countryDetails
+
+  const removeItem = () => {
+    onRemoveItem(id)
+  }
+
+  return (
+    <li className="country-list-item">
+      {isVisited ? (
+        <>
+          <img src={imageUrl} alt="thumbnail" className="country-image" />
+          <div className="country-details">
+            <p className="con-name"> {name} </p>
+            <button
+              type="button"
+              className="remove-button"
+              onClick={removeItem}
+            >
+              Remove
+            </button>
+          </div>
+        </>
+      ) : null}
+    </li>
+  )
+}
+
+export default CountryItem
